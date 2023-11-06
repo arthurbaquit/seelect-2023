bda31de60a4d968e9e3d6343170aa184eae8c8d1
Arthur <arthur.baquit@hotmail.com>
2023-11-05 23:59:50 -0300

    rm: commented code

--- client/pages/api/useMeals.ts
+++ client/pages/api/useMeals.ts
@@ -24,44 +24,9 @@ export const useMeals = () => {
       console.log(error);
     }
   }, []);
-  //   const addAtivos = useCallback(
-  //     async (ativo: Ativo) => {
-  //       if (ativos.find((a) => a.nome === ativo.nome)) {
-  //         return;
-  //       }
-  //       try {
-  //         const response = await API.post("save", ativo);
-  //         return response.data;
-  //       } catch (error) {
-  //         console.log(error);
-  //       }
-  //     },
-  //     [ativos]
-  //   );
-
-  //   const removeAtivos = useCallback(
-  //     async (ativo: Ativo) => {
-  //       try {
-  //         const response = await API.delete(`/delete/${ativo.id}`);
-  //         return response.data;
-  //       } catch (error) {
-  //         console.log(error);
-  //       }
-  //     },
-
-  //     [ativos]
-  //   );
 
   useEffect(() => {
     fetch();
   }, [fetch]);
   return { meals, fetch };
 };
-
-// export type AssetTypes =
-//   | "NationalStocks"
-//   | "ForeignStocks"
-//   | "FIIs"
-//   | "REITs"
-//   | "Cryptos"
-//   | "FixedIncome";
